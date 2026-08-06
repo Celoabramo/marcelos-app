0266544947cd40d32780506db22fadc08e0f65cb
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-06 11:28:50 -0300

    V02- Aprimorei o código

--- python.py
+++ python.py
@@ -1,1 +1,10 @@
 print("farm total de aura 6767")
+n1 = int(input("Que conta é essa professora?"))
+n2 = int(input(":"))
+n3 = int(input(":"))
+n4 = int(input(":"))
+N = n1 + n2 + n3 + n4
+if N == 67:
+    print ("ESSA É MUITO FACIL PROFESSSOAR É 67")
+else:
+    print("vtmnc")
